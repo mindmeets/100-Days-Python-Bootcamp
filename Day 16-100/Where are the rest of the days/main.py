eab58cd8011f631092dc0170933f2a87b0bdcf7b
Satya <s.satya21@gmail.com>
2025-09-03 02:01:53 +0530

    Random commit

--- Day 16-100/Where are the rest of the days/main.py
+++ Day 16-100/Where are the rest of the days/main.py
@@ -2,3 +2,8 @@
 # Instead of using templates that I have created for you.
 # It will be another step in your journey as a developer!
 # But don't worry, I will explain how to do everything in the video tutorials on Udemy.
+
+import random
+
+print("Here is a random number from 1 to 100")
+print(random.randint(1, 100))
