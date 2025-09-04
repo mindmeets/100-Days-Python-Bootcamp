c3e22de8a7e5c39ccfbc9c8e88e646d623f16e7d
Satya <s.satya21@gmail.com>
2025-09-05 00:57:39 +0530

    Day 16 complete

--- Day 16-100/Where are the rest of the days/main.py
+++ Day 16-100/Where are the rest of the days/main.py
@@ -3,7 +3,30 @@
 # It will be another step in your journey as a developer!
 # But don't worry, I will explain how to do everything in the video tutorials on Udemy.
 
-import random
+# import random
+#
+# print("Here is a random number from 1 to 100")
+# print(random.randint(1, 100))
 
-print("Here is a random number from 1 to 100")
-print(random.randint(1, 100))
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("green")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# my_screen.setup(1600, 900)
+# my_screen.bgcolor("black")
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = 'l'
+
+print(table)
+
